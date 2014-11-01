55961288a24e59b222386e2651d70ab42c0e1776
Baxter Ross <baxterross@gmail.com>
2014-10-31 23:31:11 -0700

    Adding new s3 bucket info

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,12 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => 'AKIAICLGJBCMVXOJX7AQ',
-    :aws_secret_access_key  => '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'
+    :aws_access_key_id      => 'AKIAIWS4SRSYJ66KIHIA',
+    :aws_secret_access_key  => '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'
   }
   if [Rails.env] == 'production'
-    config.fog_directory  = 'wegottago-production'
+    config.fog_directory  = 'wegottago.production'
   else
-    config.fog_directory  = 'wegottago-staging'
+    config.fog_directory  = 'wegottago.staging'
   end
 end
