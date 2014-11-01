4755f2691afd5653e8ceb25112e82e0b6e77673c
Baxter Ross <baxterross@gmail.com>
2014-11-01 16:43:46 -0700

    Using environment variables to store s3 access keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => 'AKIAIWS4SRSYJ66KIHIA',
-    :aws_secret_access_key  => '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'
+    :aws_access_key_id      => ENV['aws_id'],
+    :aws_secret_access_key  => ENV['aws_key']
   }
   if [Rails.env] == 'production'
     config.fog_directory  = 'wegottago.production'
