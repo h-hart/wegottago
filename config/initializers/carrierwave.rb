0d1f57b75532e5cf695dc76bd96dfc45c1c096ef
Baxter Ross <baxterross@gmail.com>
2014-11-01 17:02:22 -0700

    tweaking config variable names

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => ENV['aws_id'],
-    :aws_secret_access_key  => ENV['aws_key']
+    :aws_access_key_id      => ENV['S3_ID'],
+    :aws_secret_access_key  => ENV['S3_KEY']
   }
   if [Rails.env] == 'production'
     config.fog_directory  = 'wegottago.production'
