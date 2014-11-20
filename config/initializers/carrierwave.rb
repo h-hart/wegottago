134d1320e15140ec89bb8ecd2277921ed328da3a
Baxter Ross <baxterross@gmail.com>
2014-11-19 17:34:31 -0800

    Adding carrierwave s3 config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,9 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
     :aws_access_key_id      => ENV['S3_ID'],
-    :aws_secret_access_key  => ENV['S3_KEY']
+    :aws_secret_access_key  => ENV['S3_KEY'],
+    :host                   => 's3.amazonaws.com',             # optional, defaults to nil
+    :endpoint               => 'https://s3.amazonaws.com' # optional, defaults to nil
   }
   if [Rails.env] == 'production'
     config.fog_directory  = 'wegottago.production'
