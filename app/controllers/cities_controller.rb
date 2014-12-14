a1f3e54f33e54e98ae04279c0287fd57f373ebbd
Baxter Ross <baxterross@gmail.com>
2014-12-14 13:12:48 -0800

    refactoring activity search code and adding a flag to show past activities

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -11,32 +11,61 @@ class CitiesController < ApplicationController
     else
       City.find(params[:id])
     end
+    
     get_activities
 
-    render 'unauth_show' unless user_signed_in?
+    if user_signed_in?
+      render 'show'
+    else
+      render 'unauth_show'
+    end
   end
 
   private
     def get_activities
-      search = { "lat_gt"    => (@city.loc_lat - 1),
-                 "lng_gt"    => (@city.loc_lng - 1),
-                 "lat_lt"    => (@city.loc_lat + 1),
-                 "lng_lt"    => (@city.loc_lng + 2) }
-      users_search = Hash[search.map { |k, v| ["loc_#{k}", v] }]
+      search = {
+        "lat_gt" => (@city.loc_lat - 1),
+        "lng_gt" => (@city.loc_lng - 1),
+        "lat_lt" => (@city.loc_lat + 1),
+        "lng_lt" => (@city.loc_lng + 2)
+      }
+      users_search = Hash[search.map { |k, v|
+        ["loc_#{k}", v]
+      }]
+
+      time_window_end = Time.now
+      time_window_end -= 10.years if params[:show_past] == 'true'
 
       if user_signed_in?
-        @search = Activity.filter(current_user)
-                          .order('activities.end_datetime ASC')
-                          .search(search)
-        @activities = @search.paginate(:page => params[:page], :per_page => 9)
-        @users_going_places = User.joins(:activity_join).joins(:activities).includes(:activities).filter(current_user).includes(:themes).limit(4).search(users_search).all
+        @search = Activity
+          .filter(current_user)
+          .order('activities.end_datetime ASC')
+          .search(search)
+        @users_going_places = User
+          .joins(:activity_join)
+          .joins(:activities)
+          .includes(:activities)
+          .filter(current_user)
+          .includes(:themes)
+          .limit(4)
+          .search(users_search)
+          .all
       else
         @search = Activity
-        .includes(:theme)
-        .where('activities.end_datetime >= ?', Time.now)
-        .search(search)
-        @activities = @search.paginate(:page => params[:page], :per_page => 9)
-        @users_going_places = User.joins(:curiosities).includes(:activities).includes(:themes).limit(4).search(users_search).all
+          .includes(:theme)
+          .where('activities.end_datetime >= ?', time_window_end)
+          .search(search)
+        @users_going_places = User
+          .joins(:curiosities)
+          .includes(:activities)
+          .includes(:themes)
+          .limit(4)
+          .search(users_search)
+          .all
       end
+      @activities = @search.paginate(
+        page: params[:page],
+        per_page: 9
+      )
     end
 end
