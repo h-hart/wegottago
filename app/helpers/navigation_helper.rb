0f7beac75858dda3547d87f08c55e26a234b24ec
Baxter Ross <baxterross@gmail.com>
2014-12-14 12:31:28 -0800

    Adding login and signup pages to nav hgelper as external pages

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,7 +1,16 @@
 module NavigationHelper
 
   def internal_page? (url)
-    external = ['', '/'].include? url
+    external = [
+      '',
+      '/',
+      '/users/sign_in',
+      '/users/sign_up',
+      '/signup_wizard/user_info',
+      '/signup_wizard/photo',
+      '/signup_wizard/choose_interests',
+      '/signup_wizard/confirm_profile'
+    ].include? url
     !external
   end
 
